33c44058916fab17690276ab6b267a5577b53255
nishantSanandiyaAgile <nishant.sanandiya@agileinfoways.com>
2023-07-13 17:55:30 +0530

    Done with env Setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import {envName} from './src/constants';
 
-function App(): JSX.Element {
-  return <></>;
-}
+const App = () => {
+  console.log('envName :- ', envName);
+  return (
+    <SafeAreaView>
+      <Text>envName :- {envName}</Text>
+    </SafeAreaView>
+  );
+};
 
 export default App;
